f35106d1e2b24dee03f5ef1ea6133696d448150c
Alice Genkin <agenkin1@gmail.com>
2021-01-10 09:20:52 -0500

    commit tree eed576003a3ceb87bab3bb03a0f030b218b86e77

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,30 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do
+    #should use Active Record to grab all of the articles and store them in an instance variable, @articles
+    @articles = Article.all
+    erb :index
+  end
+
+  post '/articles' do
+    #creates a new article from the params from the form, then redirects to that new article's show page.
+    @article = Article.create(params)
+    erb :show
+  end
+
+
+  get '/articles/:id' do 
+    id = params[:id]
+    @article = Article.find_by(id:id)
+    #use Active Record to grab the article with the id that is in the params and set it equal to @article
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+
+
 end
